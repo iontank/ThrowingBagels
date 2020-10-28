f0737de4d32246b771959a04b6df2c3a83f2a56c
Remy Porter <rp@iontank.com>
2020-10-28 11:00:19 -0400

    Moved all the DTS/Uboot stuff to the install script
    It was kinda dumb to have that functionality in the provision script. Now the install script fully configures the system, and you just use provision to easily change things like hostname/leds.config

--- install.sh
+++ install.sh
@@ -5,4 +5,21 @@ sudo chown -R $USER /opt/ThrowingBagels
 cd /opt/ThrowingBagels
 make
 
-echo "From /opt/ThrowingBagels run scripts/provision to complete the installation."
+echo "Installing the bagel uBoot config."
+sudo sed -i.bak "s/#disable_uboot_overlay_video=1/disable_uboot_overlay_video=1/g" /boot/uEnv.txt
+sudo sed -i.bak "s/#disable_uboot_overlay_audio=1/disable_uboot_overlay_audio=1/g" /boot/uEnv.txt
+
+echo "Updating the device tree."
+sudo cp dts/*.dtbo /lib/firmware
+
+echo "This script assumes that your uEnv.txt enables the PRU, which should be the default. Ensure that a uboot_overlay_pru is set."
+echo "If not, manually change it."
+
+echo "Installing services"
+sudo scripts/setup_services
+
+echo "Throwing Bagels has been installed. Services are installed, but not enabled. Check the READMe for more."
+echo "Please reboot before trying the included software."
+echo "After a reboot, you can use systemctl to enable/start either multidemo or led-udp"
+
+echo "From /opt/ThrowingBagels run scripts/provision to configure your installation."
